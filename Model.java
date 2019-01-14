7fdb22f50c54736a0492048cd079535ce0e59f59
Jmannn <37675265+Jmannn@users.noreply.github.com>
2019-01-14 20:43:50 +1300

    Progress on hashtable model

--- Model.java
+++ Model.java
@@ -1,1 +1,31 @@
-//hashtable logic goes here
+import java.util.ArrayList;
+/* The model for a GUI representation of hashtable.
+ * @author Johnny Mann & Anaru Hudson
+ */
+public class Model{
+    /* Max size of HashTable */
+    public static final MAX_SIZE = 20;
+    /* Stores hash table entries in an array of arraylists
+     * to allow for chaining via arraylist
+     */
+    public static ArrayList<String>[] hashArray;
+
+    public Model(int size){
+    };
+
+    public void resize(int size){
+	//create new table
+	//rehash it
+
+    }
+    /* Turns a string into a key to be hashed */
+    private int toKey(String value){
+	return value.hasCode();
+    }
+    /* Calculates hash from calc'd key and stores element */
+    public void addElement(String value){
+    }
+    public void changeKey(int){
+    }
+
+}
--- View.java
+++ View.java
@@ -1,1 +1,6 @@
 //View and associated stuff goes here
+
+//use tableview to represent the hashtable
+// first column is indexNumber
+// second column is first value
+// each addition column holds the chained enteries
