af4bbf23cdb6a8369c830c8516b11983488b256d
Jmannn <37675265+Jmannn@users.noreply.github.com>
2019-01-27 16:43:44 +1300

    Mostly works

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Basic".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GuiHashTable.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            GuiHashTable g = new GuiHashTable();
+            Model m = new Model(20);
+            Controller c = new Controller(g, m);
+            g.setVisible(true);
+        });
+    }
+}
